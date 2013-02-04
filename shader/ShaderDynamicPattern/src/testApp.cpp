4ee1bb4304d72f5482161aaef0d120a7ccfdc602
kynd <mail@kynd.info>
2013-02-04 16:58:53 +0900

    some more patterns aded to ShaderDynamicPattern

--- shader/ShaderDynamicPattern/src/testApp.cpp
+++ shader/ShaderDynamicPattern/src/testApp.cpp
@@ -237,6 +237,291 @@ void testApp::setup(){
    
     //--------------------------------------------------------------
     
+    shader = new ofShader();
+    shaderProgram = STRINGIFY(
+                              uniform sampler2DRect tex0;
+                              uniform sampler2DRect tex1;
+                              uniform int numParticles;
+                              uniform int width;
+                              uniform int height;
+                              void main (void){
+                                  bool flag = false;
+                                  int ind0 = 0;
+                                  int ind1 = 0;
+                                  int ind2 = 0;
+                                  float minDist = 2000.0;
+                                  for (int i = 0; i < numParticles; i ++) {
+                                      vec2 posSample = texture2DRect(tex1, vec2(i, 0) + vec2(0.5, 0.5)).rg;
+                                      posSample.r *= float(width);
+                                      posSample.g *= float(height);
+                                      float dist = distance(posSample, gl_TexCoord[0].st);
+                                      if (dist < minDist) {
+                                          ind2 = ind1; ind1 = ind0; ind0 = i;
+                                          minDist = dist;
+                                      }
+                                      if (dist < 3.0) {
+                                          flag = true;
+                                      }
+                                  }
+                                  float r = float(ind0) / float(numParticles);
+                                  float g = float(ind1) / float(numParticles);
+                                  float b = float(ind2) / float(numParticles);
+                                  gl_FragColor = (flag) ? vec4(1.0, 1.0, 0.0, 1.0) : vec4(r, g, b, 1.0);
+                              }
+                              );
+    
+    
+    
+    shader->setupShaderFromSource(GL_FRAGMENT_SHADER, shaderProgram);
+    shader->linkProgram();
+    shaders.push_back(shader);
+    
+    //--------------------------------------------------------------
+    
+    shader = new ofShader();
+    shaderProgram = STRINGIFY(
+                              uniform sampler2DRect tex0;
+                              uniform sampler2DRect tex1;
+                              uniform int numParticles;
+                              uniform int width;
+                              uniform int height;
+                              void main (void){
+                                  bool flag = false;
+                                  float PI = 3.14159265;
+                                  float rSum = 0.0;
+                                  float gSum = 0.0;
+                                  float bSum = 0.0;
+                                  for (int i = 0; i < numParticles; i ++) {
+                                      vec2 posSample = texture2DRect(tex1, vec2(i, 0) + vec2(0.5, 0.5)).rg;
+                                      posSample.r *= float(width);
+                                      posSample.g *= float(height);
+                                      float dist = distance(posSample, gl_TexCoord[0].st);
+                                      float angle = float(i) / float(numParticles) * PI;
+                                      float adj = 1.0 / float(numParticles) / dist * 100.0;
+                                      rSum += (sin(angle) + 1.0) / 2.0 * adj;
+                                      gSum += (sin(angle + PI / 3.0) + 1.0) / 2.0 * adj;
+                                      bSum += (sin(angle + PI / 3.0) + 1.0) / 2.0 * adj;
+                                      if (dist < 3.0) {
+                                          flag = true;
+                                      }
+                                  }
+                                  gl_FragColor = (flag) ? vec4(1.0, 1.0, 0.0, 1.0) : vec4(rSum, gSum, bSum, 1.0);
+                              }
+                              );
+    
+    
+    
+    shader->setupShaderFromSource(GL_FRAGMENT_SHADER, shaderProgram);
+    shader->linkProgram();
+    shaders.push_back(shader);
+    
+    //--------------------------------------------------------------
+    
+    shader = new ofShader();
+    shaderProgram = STRINGIFY(
+                              uniform sampler2DRect tex0;
+                              uniform sampler2DRect tex1;
+                              uniform int numParticles;
+                              uniform int width;
+                              uniform int height;
+                              void main (void){
+                                  bool flag = false;
+                                  float PI = 3.14159265;
+                                  vec2 pos0 = vec2(0.0);
+                                  vec2 pos1 = vec2(0.0);
+                                  float minDist = 2000.0;
+                                  for (int i = 0; i < numParticles; i ++) {
+                                      vec2 posSample = texture2DRect(tex1, vec2(i, 0) + vec2(0.5, 0.5)).rg;
+                                      posSample.r *= float(width);
+                                      posSample.g *= float(height);
+                                      float dist = distance(posSample, gl_TexCoord[0].st);
+                                      if (dist < minDist) {
+                                          pos1 = pos0;
+                                          pos0 = posSample;
+                                          minDist = dist;
+                                      }
+                                      if (dist < 3.0) {
+                                          flag = true;
+                                      }
+                                  }
+                                  vec2 average = (pos0 + pos1) / 2.0;
+                                  float angle = atan(average.r - gl_TexCoord[0].s, average.g- gl_TexCoord[0].t);
+                                  
+                                  float r = (sin(angle) + 1.0) / 2.0 / max(1.0, minDist * 0.05);
+                                  float g = (sin(angle + PI / 3.0) + 1.0) / 2.0 / max(1.0, minDist * 0.05);
+                                  float b = (sin(angle + PI / 3.0 * 2.0) + 1.0) / 2.0 / max(1.0, minDist * 0.05);
+                                  gl_FragColor = (flag) ? vec4(1.0, 1.0, 0.0, 1.0) : vec4(r, g, b, 1.0);
+                              }
+                              );
+    
+    
+    
+    shader->setupShaderFromSource(GL_FRAGMENT_SHADER, shaderProgram);
+    shader->linkProgram();
+    shaders.push_back(shader);
+   
+    //--------------------------------------------------------------
+    
+    shader = new ofShader();
+    shaderProgram = STRINGIFY(
+                              uniform sampler2DRect tex0;
+                              uniform sampler2DRect tex1;
+                              uniform int numParticles;
+                              uniform int width;
+                              uniform int height;
+                              void main (void){
+                                  bool flag = false;
+                                  float PI = 3.14159265;
+                                  float rSum = 0.0;
+                                  float gSum = 0.0;
+                                  float bSum = 0.0;
+                                  for (int i = 0; i < numParticles; i ++) {
+                                      vec2 posSample = texture2DRect(tex1, vec2(i, 0) + vec2(0.5, 0.5)).rg;
+                                      posSample.r *= float(width);
+                                      posSample.g *= float(height);
+                                      float dist = distance(posSample, gl_TexCoord[0].st);
+                                      float angle = float(i) / float(numParticles) * PI;
+                                      float adj = 1.0 / float(numParticles) / dist * 100.0;
+                                      rSum += (sin(angle) + 1.0) / 2.0 * adj;
+                                      gSum += (sin(angle + PI / 3.0) + 1.0) / 2.0 * adj;
+                                      bSum += (sin(angle + PI / 3.0) + 1.0) / 2.0 * adj;
+                                      if (dist < 3.0) {
+                                          flag = true;
+                                      }
+                                  }
+                                  float r = sin(rSum * PI * 5.0);
+                                  float g = sin(gSum * PI * 5.0);
+                                  float b = sin(bSum * PI * 5.0);
+                                  gl_FragColor = (flag) ? vec4(1.0, 1.0, 0.0, 1.0) : vec4(r, g, b, 1.0);
+                              }
+                              );
+    
+    
+    
+    shader->setupShaderFromSource(GL_FRAGMENT_SHADER, shaderProgram);
+    shader->linkProgram();
+    shaders.push_back(shader);
+    
+    //--------------------------------------------------------------
+    
+    shader = new ofShader();
+    shaderProgram = STRINGIFY(
+                              uniform sampler2DRect tex0;
+                              uniform sampler2DRect tex1;
+                              uniform int numParticles;
+                              uniform int width;
+                              uniform int height;
+                              void main (void){
+                                  bool flag = false;
+                                  float PI = 3.14159265;
+                                  float r = 0.0;
+                                  float g = 0.0;
+                                  float b = 0.0;
+                                  for (int i = 0; i < numParticles; i ++) {
+                                      vec2 posSample = texture2DRect(tex1, vec2(i, 0) + vec2(0.5, 0.5)).rg;
+                                      posSample.r *= float(width);
+                                      posSample.g *= float(height);
+                                      float dist = distance(posSample, gl_TexCoord[0].st);
+                                      if (abs(posSample.r - gl_TexCoord[0].s) < 2.0) {
+                                          r += 0.3;
+                                          b += 0.2;
+                                      }
+                                      if (abs(posSample.g - gl_TexCoord[0].t) < 2.0) {
+                                          g += 0.3;
+                                          b += 0.2;
+                                      }
+                                      if (dist < 3.0) {
+                                          flag = true;
+                                      }
+                                  }
+                                  gl_FragColor = (flag) ? vec4(1.0, 1.0, 0.0, 1.0) : vec4(r, g, b, 1.0);
+                              }
+                              );
+    
+    
+    
+    shader->setupShaderFromSource(GL_FRAGMENT_SHADER, shaderProgram);
+    shader->linkProgram();
+    shaders.push_back(shader);
+    
+    //--------------------------------------------------------------
+    
+    shader = new ofShader();
+    shaderProgram = STRINGIFY(
+                              uniform sampler2DRect tex0;
+                              uniform sampler2DRect tex1;
+                              uniform int numParticles;
+                              uniform int width;
+                              uniform int height;
+                              void main (void){
+                                  bool flag = false;
+                                  float PI = 3.14159265;
+                                  float r = 0.0;
+                                  float g = 0.0;
+                                  float b = 0.0;
+                                  for (int i = 0; i < numParticles; i ++) {
+                                      vec2 posSample = texture2DRect(tex1, vec2(i, 0) + vec2(0.5, 0.5)).rg;
+                                      posSample.r *= float(width);
+                                      posSample.g *= float(height);
+                                      float dist = distance(posSample, gl_TexCoord[0].st);
+                                      r += sin((posSample.r - gl_TexCoord[0].s) * 0.002 * float(i)) * 0.05;
+                                      g += sin((posSample.g - gl_TexCoord[0].t) * 0.002 * float(i)) * 0.05;
+                                      b += sin(r + g) * 0.2;
+                                      if (dist < 3.0) {
+                                          flag = true;
+                                      }
+                                  }
+                                  gl_FragColor = (flag) ? vec4(1.0, 1.0, 0.0, 1.0) : vec4(r, g, b, 1.0);
+                              }
+                              );
+    
+    
+    
+    shader->setupShaderFromSource(GL_FRAGMENT_SHADER, shaderProgram);
+    shader->linkProgram();
+    shaders.push_back(shader);
+    
+    //--------------------------------------------------------------
+    
+    shader = new ofShader();
+    shaderProgram = STRINGIFY(
+                              uniform sampler2DRect tex0;
+                              uniform sampler2DRect tex1;
+                              uniform int numParticles;
+                              uniform int width;
+                              uniform int height;
+                              void main (void){
+                                  bool flag = false;
+                                  float PI = 3.14159265;
+                                  float r = 0.0;
+                                  float g = 0.0;
+                                  float b = 0.0;
+                                  for (int i = 0; i < numParticles; i ++) {
+                                      vec2 posSample = texture2DRect(tex1, vec2(i, 0) + vec2(0.5, 0.5)).rg;
+                                      posSample.r *= float(width);
+                                      posSample.g *= float(height);
+                                      float dist = distance(posSample, gl_TexCoord[0].st);
+                                      r += sin((posSample.r - gl_TexCoord[0].s) * 0.001 * float(i)) * 0.03;
+                                      g += sin((posSample.g - gl_TexCoord[0].t) * 0.001 * float(i)) * 0.03;
+                                      b += sin(r + g) * 0.2;
+                                      if (dist < 3.0) {
+                                          flag = true;
+                                      }
+                                  }
+                                  r = min(r, b);
+                                  g = min(g, b);
+                                  gl_FragColor = (flag) ? vec4(1.0, 1.0, 0.0, 1.0) : vec4(r, g, b, 1.0);
+                              }
+                              );
+    
+    
+    
+    shader->setupShaderFromSource(GL_FRAGMENT_SHADER, shaderProgram);
+    shader->linkProgram();
+    shaders.push_back(shader);
+    
+
+    //--------------------------------------------------------------
     shaderIndex = 0;
     renderImage.allocate(ofGetWidth(), ofGetHeight(), OF_IMAGE_COLOR);
 }
